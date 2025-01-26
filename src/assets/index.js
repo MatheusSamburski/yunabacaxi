440ebd891f59df00191aa2f77d13b05f50b81ce1
MatheusSamburski <mateus41789@gmail.com>
2025-01-26 16:11:03 -0300

    projeto com routes

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import fotoYuna from "./fotoYuna.png"
+import logoDiscord from "./logoDiscord.png"
+import logoX from "./logoX.png"
+import logoKofi from "./logoKofi.png"
+import logoTwitch from "./logoTwitch.png"
+import logoSteam from "./logoSteam.png"
+import logoSpotify from "./logoSpotify.png"
+import iconeHome from "./iconeHome.png"
+import iconeFlor from "./iconeFlor.png"
+import iconeEmail from "./iconeEmail.png"
+import iconeCalendario from "./iconeCalendario.png"
+import logoYunabacaxi from "./logoYunabacaxi.png"
+
+export {
+  iconeFlor,
+  iconeEmail,
+  iconeCalendario,
+  logoYunabacaxi,
+  fotoYuna,
+  logoDiscord,
+  logoKofi,
+  logoSpotify,
+  logoSteam,
+  logoTwitch,
+  logoX,
+  iconeHome
+}
